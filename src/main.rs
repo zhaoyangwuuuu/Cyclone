f5b31a3e6b30a993860faba3702ba712d6c6e9a0
zhaoyangwu <zhaoyang1018@hotmail.com>
2023-05-17 23:17:49 -0400

    add import

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// -*- compile-command: "cargo build" -*-
+#[macro_use]
+extern crate clap;
+extern crate core;
+#[macro_use]
+extern crate error_chain;
+extern crate time;
+extern crate walkdir;
+
 fn main() {
     println!("Hello, world!");
 }
