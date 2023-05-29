3d5c0f784008e9fd76d5dac121a75e82da63c998
zhaoyangwu <zhaoyang1018@hotmail.com>
2023-05-28 22:08:57 -0400

    add commands

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
-// -*- compile-command: "cargo build" -*-
 #[macro_use]
-extern crate clap;
 extern crate core;
 #[macro_use]
 extern crate error_chain;
 extern crate time;
 extern crate walkdir;
 
-fn main() {
-    println!("Hello, world!");
+use clap::{Args, Parser, Subcommand};
+use std::io::{BufRead, BufReader, Read, Write};
+use std::os::unix::fs::{FileTypeExt, PermissionsExt};
+use std::path::{Path, PathBuf};
+use std::{env, fs, io};
+use walkdir::WalkDir;
+mod errors {
+    error_chain! {}
 }
+use errors::*;
+
+#[derive(Parser)]
+#[command(author = "Zhaoyang", about, version)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Graveyard(Graveyard),
+}
+
+#[derive(Args)]
+struct Graveyard {}
+
+#[derive(Args)]
+struct Decompose {}
+
+#[derive(Args)]
+struct Seance {}
+
+#[derive(Args)]
+struct Unbury {}
+
+#[derive(Args)]
+struct Inspect {}
+fn main() {}
