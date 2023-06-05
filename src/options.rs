1173b269146c332107d88fb215156079c617d45c
zhaoyangwu <zhaoyang1018@hotmail.com>
2023-06-04 22:04:23 -0400

    move preview to a function by itself

--- src/options.rs
+++ src/options.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
 use std::env;
-use std::fs;
+use std::fs::{self, Metadata};
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use walkdir::WalkDir;
@@ -28,48 +28,53 @@ pub fn delete(file: &str, cli: &Cli) -> Result<()> {
 
         // Check if preview is enabled
         if cli.preview {
-            if metadata.is_dir() {
-                // Get the size of the directory and all its contents
-                println!(
-                    "{}: directory, {} including:",
-                    file,
-                    humanize_bytes(
-                        WalkDir::new(source)
-                            .into_iter()
-                            .filter_map(|x| x.ok())
-                            .filter_map(|x| x.metadata().ok())
-                            .map(|x| x.len())
-                            .sum::<u64>()
-                    )
-                );
-
-                // Print the first few files in the directory
-                for entry in WalkDir::new(source)
-                    .min_depth(1)
-                    .max_depth(1)
-                    .into_iter()
-                    .filter_map(|entry| entry.ok())
-                    .take(FILES_TO_INSPECT)
-                {
-                    println!("{}", entry.path().display());
-                }
-            } else {
-                println!("{}: file, {}", file, humanize_bytes(metadata.len()));
-                // Read the file and print the first few lines
-                if let Ok(f) = fs::File::open(source) {
-                    for line in BufReader::new(f)
-                        .lines()
-                        .take(LINES_TO_INSPECT)
-                        .filter_map(|line| line.ok())
-                    {
-                        println!("> {}", line);
-                    }
-                } else {
-                    println!("Error reading {}", source.display());
-                }
-            }
+            preview(&metadata, source, file);
         }
     }
 
     Ok(())
+}
+
+// Preview the changes without actually deleting the files
+fn preview(metadata: &Metadata, source: &PathBuf, file: &str) {
+    if metadata.is_dir() {
+        // Get the size of the directory and all its contents
+        println!(
+            "{}: directory, {} including:",
+            file,
+            humanize_bytes(
+                WalkDir::new(source)
+                    .into_iter()
+                    .filter_map(|x| x.ok())
+                    .filter_map(|x| x.metadata().ok())
+                    .map(|x| x.len())
+                    .sum::<u64>()
+            )
+        );
+
+        // Print the first few files in the directory
+        for entry in WalkDir::new(source)
+            .min_depth(1)
+            .max_depth(1)
+            .into_iter()
+            .filter_map(|entry| entry.ok())
+            .take(FILES_TO_INSPECT)
+        {
+            println!("{}", entry.path().display());
+        }
+    } else {
+        println!("{}: file, {}", file, humanize_bytes(metadata.len()));
+        // Read the file and print the first few lines
+        if let Ok(f) = fs::File::open(source) {
+            for line in BufReader::new(f)
+                .lines()
+                .take(LINES_TO_INSPECT)
+                .filter_map(|line| line.ok())
+            {
+                println!("> {}", line);
+            }
+        } else {
+            println!("Error reading {}", source.display());
+        }
+    }
 }
